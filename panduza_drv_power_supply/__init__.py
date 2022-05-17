c7e94e3a32d80e88afefcacc8613d81bb6c071bb
Antoine Gouby <antoine.gouby@protonmail.com>
2022-05-17 20:57:56 +0200

    Added ka005p driver

--- panduza_drv_power_supply/__init__.py
+++ panduza_drv_power_supply/__init__.py
@@ -1,7 +1,9 @@
 from .driver_hm7044 import DriverHm7044
+from .driver_rnd320_ka005p import DriverRnd320_KA005P
 
 PZA_DRIVERS_LIST=[
-    DriverHm7044
+    DriverHm7044,
+    DriverRnd320_KA005P
 ]
 
 
